59fa20377f0171c109c6de033952f3e7debbd7a0
de_roesta <jackfadhli32@gmail.com>
2022-10-25 17:10:53 +0700

    first commit

---
+++ kode/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <Nav></Nav>
+  </div>
+</template>
+<script>
+import Nav from '~/components/Nav.vue';
+export default {
+    components: { Nav }
+};
+</script>
+<style>
+  
+</style>
